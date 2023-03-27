a00a31486338acc82353bc65674b06e0689642ea
Aubrey Chen <aubrey_chen@foxmail.com>
2023-03-27 17:18:42 +0800

    项目骨架搭建成功

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <div>Bot昵称： {{ bot_name }}</div>
+    <div>Bot战力： {{ bot_rating }}</div>
+  </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+// 接受一个参数值并返回一个响应式且可改变的ref对象。rdf对象拥有一个指向内部值的单一属性.value。
+import { ref } from 'vue';  // Vue3中通过ref来声明变量
+
+// export default相当于创建了一个vue实例
+export default {
+  name: "App", 
+  setup: () => {  // setup是将里面的变量函数中返回的对象暴露给模板和组件实例
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    // 访问后端链接，用Ajax来写
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",  // 函数地址
+      type: "get",   // 获取一个数据
+      // 成功后获取resp对象
+      success: resp => {
+        // console.log(resp);  // 获取后端数据
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+
+    return {
+      bot_name, 
+      bot_rating
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("@/assets/background.png");  /* @/···表示在当前目录的根目录 */
+  background-size: cover;
 }
 </style>
