41b7cb27134c062f315a8215dfd2f8295f39ded0
Aubrey Chen <aubrey_chen@foxmail.com>
2023-05-10 16:41:30 +0800

    前端框架单页面的应用：前后端分离的前端框架，可以实现点击各页面跳转时页面不刷新。将NavBar.vue里的<a>标签改写成<router-link>标签，并把其中通过href属性跳转的链接改写成通过:to绑定的“{name: ''}”路由名称。前端渲染的原理就是字符串的处理。

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -2,20 +2,20 @@
   <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary"></nav>顶部透明色主题的导航栏写法 -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">    <!--  顶部白色主题的导航栏写法，把两处的dark都换成light实现深色主题的导航栏 -->
     <div class="container">
-      <a class="navbar-brand" href="/">King Of Bots</a>
+      <router-link class="navbar-brand" :to="{name: 'home'}">King Of Bots</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/pk/">对战</a>
+            <router-link class="nav-link" aria-current="page" :to="{name: 'pk_index'}">对战</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/record/">对局列表</a>
+            <router-link class="nav-link" :to="{name: 'record_index'}">对局列表</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/ranklist/">排行榜</a>
+            <router-link class="nav-link" :to="{name: 'ranklist_index'}">排行榜</router-link>
           </li>
         </ul>
         <ul class="navbar-nav">
@@ -24,7 +24,7 @@
               Aubrey Chen
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="/user/bot">我的Bots</a></li>
+              <li><router-link class="dropdown-item" :to="{name: 'user_bot_index'}">我的Bot</router-link></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">退出</a></li>
             </ul>
