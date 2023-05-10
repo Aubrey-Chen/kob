7694b3e6db799588073df313279c3e42cce1d2c1
Aubrey Chen <aubrey_chen@foxmail.com>
2023-05-10 11:50:07 +0800

    在Vue工作台的依赖里安装@popperjs/core依赖。导航栏可以复用 ，独立出来作为一个组件，使用Bootstrap的方式创建导航栏。

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,39 +1,16 @@
 <template>
-  <div>
-    <div>Bot昵称： {{ bot_name }}</div>
-    <div>Bot战力： {{ bot_rating }}</div>
-  </div>
+  <NavBar />
   <router-view/>
 </template>
 
 <script>
-import $ from 'jquery';
-// 接受一个参数值并返回一个响应式且可改变的ref对象。rdf对象拥有一个指向内部值的单一属性.value。
-import { ref } from 'vue';  // Vue3中通过ref来声明变量
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import NavBar from '@/components/NavBar.vue';
 
-// export default相当于创建了一个vue实例
 export default {
-  name: "App", 
-  setup: () => {  // setup是将里面的变量函数中返回的对象暴露给模板和组件实例
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    // 访问后端链接，用Ajax来写
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",  // 函数地址
-      type: "get",   // 获取一个数据
-      // 成功后获取resp对象
-      success: resp => {
-        // console.log(resp);  // 获取后端数据
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name, 
-      bot_rating
-    }
+  components: {
+    NavBar, 
   }
 }
 </script>
---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary"></nav>顶部透明色主题的导航栏写法 -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">    <!--  顶部白色主题的导航栏写法，把两处的dark都换成light实现深色主题的导航栏 -->
+    <div class="container">
+      <a class="navbar-brand" href="#">King Of Bots</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">对战</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">对局列表</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">排行榜</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Aubrey Chen
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item" href="#">我的Bots</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">退出</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+</script>
+
+<!-- <style>后加scoped的作用：在这个里面写的CSS它会加上一个随机字符串，使得这个样式不会影响到组件以外的部分 -->
+<style scoped>
+
+</style>
