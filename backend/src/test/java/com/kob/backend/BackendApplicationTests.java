7ad005838753e4ca3ec33bc2bd130f1587a5ea04
Aubrey Chen <aubrey_chen@foxmail.com>
2023-07-06 20:25:49 +0800

    实现config.SecurityConfig类，使用BCrypt算法对用户密码进行哈希和加密存储，以及实现UserController里新增用户时的密码加密存储功能

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,20 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("pcpr"));
+        System.out.println(passwordEncoder.matches("pcpr", "$2a$10$JxUzSOQpgbP9RP5duGKQl.CUXeWsuikkTA/WBpSWbGJQFY2s1W9R."));
+        System.out.println(passwordEncoder.encode("pcq"));
+        System.out.println(passwordEncoder.encode("pczq"));
+        System.out.println(passwordEncoder.encode("phsc"));
     }
 
 }
